6f96041b1ffb20390978b1af1739e27c2e64d4f3
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-06-01 21:06:58 +0200

    Some work has been done

--- src/interval.cpp
+++ src/interval.cpp
@@ -36,113 +36,155 @@ using namespace Gecode::Int;
  * Custom brancher for forcing mandatory parts
  *
  */
-class IntervalBrancher : public Brancher {
+class IntervalBrancher: public Brancher {
 protected:
-  // Views for x-coordinates (or y-coordinates)
-  ViewArray<IntView> x;
-  // Width (or height) of rectangles
-  int* w;
-  // Percentage for obligatory part
-  double p;
-  // Cache of first unassigned view
-  mutable int start;
-  // Description
-  class Description : public Choice {
-  public:
-    // Position of view
-    int pos;
-    // You might need more information, please add here
+	// Views for x-coordinates (or y-coordinates)
+	ViewArray<IntView> x;
+	// Width (or height) of rectangles
+	int* w;
+	// Percentage for obligatory part
+	double p;
+	// Cache of first unassigned view
+	mutable int start;
+	// Description
+	class Description: public Choice {
+	public:
+		// Position of view
+		int pos;
+		int obligMin;
+		int obligMax;
+		// You might need more information, please add here
 
-    /* Initialize description for brancher b, number of
-     *  alternatives a, position p, and ???.
-     */
-    Description(const Brancher& b, unsigned int a, int p)
-      : Choice(b,a), pos(p) {}
-    // Report size occupied
-    virtual size_t size(void) const {
-      return sizeof(Description);
-    }
-    // Archive the choice's information in e
-    virtual void archive(Archive& e) const {
-      Choice::archive(e);
-      // You must also archive the additional information
-      e << pos << ...;
-    }
-  };
+		/* Initialize description for brancher b, number of
+		 *  alternatives a, position p, and ???.
+		 */
+		Description(const Brancher& b, unsigned int a, int p, int stMin,
+				int stMax) :
+				Choice(b, a), pos(p), obligMin(stMin), obligMax(stMax) {
+		}
+		// Report size occupied
+		virtual size_t size(void) const {
+			return sizeof(Description);
+		}
+		// Archive the choice's information in e
+		virtual void archive(Archive& e) const {
+			Choice::archive(e);
+			// You must also archive the additional information
+			e << pos << obligMin << obligMax;
+		}
+	};
 public:
-  // Construct branching
-  IntervalBrancher(Home home, 
-                   ViewArray<IntView>& x0, int w0[], double p0)
-    : Brancher(home), x(x0), w(w0), p(p0), start(0) {}
-  // Post branching
-  static void post(Home home, ViewArray<IntView>& x, int w[], double p) {
-    (void) new (home) IntervalBrancher(home,x,w,p);
-  }
+	// Construct branching
+	IntervalBrancher(Home home, ViewArray<IntView>& x0, int w0[], double p0) :
+			Brancher(home), x(x0), w(w0), p(p0), start(0) {
+	}
+	// Post branching
+	static void post(Home home, ViewArray<IntView>& x, int w[], double p) {
+		(void) new (home) IntervalBrancher(home, x, w, p);
+	}
 
-  // Copy constructor used during cloning of b
-  IntervalBrancher(Space& home, bool share, IntervalBrancher& b)
-    : Brancher(home, share, b), p(b.p), start(b.start) {
-    x.update(home,share,b.x);
-    w = home.alloc<int>(x.size());
-    for (int i=x.size(); i--; )
-      w[i]=b.w[i];
-  }
-  // Copy brancher
-  virtual Actor* copy(Space& home, bool share) {
-    return new (home) IntervalBrancher(home, share, *this);
-  }
+	// Copy constructor used during cloning of b
+	IntervalBrancher(Space& home, bool share, IntervalBrancher& b) :
+			Brancher(home, share, b), p(b.p), start(b.start) {
+		x.update(home, share, b.x);
+		w = home.alloc<int>(x.size());
+		for (int i = x.size(); i--;)
+			w[i] = b.w[i];
+	}
+	// Copy brancher
+	virtual Actor* copy(Space& home, bool share) {
+		return new (home) IntervalBrancher(home, share, *this);
+	}
 
-  // Check status of brancher, return true if alternatives left
-  virtual bool status(const Space& home) const {
+	// Check status of brancher, return true if alternatives left
+	virtual bool status(const Space& home) const {
+		// FILL IN HERE
+		for (int i = start; i < x.size(); i++) {
+			if (!x[i].assigned() && (x[i].max() - x[i].min() >= ceil(p * w[i]))) {
+				std::cout << "Status IF" << std::endl;
+				start = i;
+				return true;
+			}
+		}
 
-    // FILL IN HERE
+		std::cout << "STATUS RETURN FALSE" << std::endl;
 
-  }
-  // Return choice as description
-  virtual const Choice* choice(Space& home) {
+		return false;
+	}
 
-    // FILL IN HERE
+	// Return choice as description
+	virtual const Choice* choice(Space& home) {
+		// FILL IN HERE
 
-  }
-  // Construct choice from archive e
-  virtual const Choice* choice(const Space&, Archive& e) {
-    // Again, you have to take care of the additional information
-    int pos, ...;
-    e >> pos >> ...;
-    return new Description(*this, pos, ...);
-  }
-  // Perform commit for choice c and alternative a
-  virtual ExecStatus commit(Space& home, 
-                            const Choice& c,
-                            unsigned int a) {
-    const Description& d = static_cast<const Description&>(c);
+		int obligEnd = x[start].min() + ceil(p * w[start]);
+		x[start].gq(home, x[start].min());
+		x[start].le(home, obligEnd);
 
-    // FILL IN HERE
+		std::cout << "START: " << start << std::endl;
+		std::cout << "x min ->" << x[start].min() << "x max ->" << obligEnd << std::endl;
 
-  }
-  // Print some information on stream o (used by Gist, from Gecode 4.0.1 on)
-  virtual void print(const Space& home, const Choice& c, unsigned int b,
-                     std::ostream& o) const {
+		return new Description(*this, 2, start, x[start].min(), obligEnd);
 
-    // FILL IN HERE
+	}
 
-  }
+	// Construct choice from archive e
+	virtual const Choice* choice(const Space&, Archive& e) {
+		// Again, you have to take care of the additional information
+	int pos, stMin, stMax;
+	e >> pos >> stMin >> stMax;
+	return new Description(*this, 2, pos, stMin, stMax);
+}
+// Perform commit for choice c and alternative a
+virtual ExecStatus commit(Space& home, const Choice& c, unsigned int a) {
+	const Description& d = static_cast<const Description&>(c);
+	// FILL IN HERE
+	int pos = d.pos;
+	int stMin = d.obligMin;
+	int stMax = d.obligMax;
+
+	if (a == 0) {
+		bool min = me_failed(x[pos].gq(home, stMin));
+		bool max = me_failed(x[pos].le(home, stMax));
+
+		return (min && max) ? ES_FAILED : ES_OK;
+	} else {
+		return me_failed(x[pos].gq(home, stMin)) ? ES_FAILED : ES_OK;
+	}
+
+}
+// Print some information on stream o (used by Gist, from Gecode 4.0.1 on)
+virtual void print(const Space& home, const Choice& c, unsigned int b,
+		std::ostream& o) const {
+	// FILL IN HERE
+	const Description& d = static_cast<const Description&>(c);
+	int pos = d.pos;
+	int stMin = d.obligMin;
+	int stMax = d.obligMax;
+
+	if (b == 0) {
+		o << "[" << pos << "] ->" << "{" << stMin << "..." << stMax << "}";
+	} else {
+		o << "b != 2";
+	}
+
+}
 };
 
 // This posts the interval branching
 void interval(Home home, const IntVarArgs& x, const IntArgs& w, double p) {
-  // Check whether arguments make sense
-  if (x.size() != w.size())
-    throw ArgumentSizeMismatch("interval");
-  // Never post a branching in a failed space
-  if (home.failed()) return;
-  // Create an array of integer views
-  ViewArray<IntView> vx(home,x);
-  // Create an array of integers
-  int* wc = static_cast<Space&>(home).alloc<int>(x.size());
-  for (int i=x.size(); i--; )
-    wc[i]=w[i];
-  // Post the brancher
-  IntervalBrancher::post(home,vx,wc,p);
+	// Check whether arguments make sense
+	if (x.size() != w.size())
+		throw ArgumentSizeMismatch("interval");
+	// Never post a branching in a failed space
+	if (home.failed())
+		return;
+	// Create an array of integer views
+	ViewArray<IntView> vx(home, x);
+	// Create an array of integers
+	int* wc = static_cast<Space&>(home).alloc<int>(x.size());
+	for (int i = x.size(); i--;)
+		wc[i] = w[i];
+	// Post the brancher
+	IntervalBrancher::post(home, vx, wc, p);
 }
 
