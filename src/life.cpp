24550e2248b4bd47b45c075cc8c773262b1b4f4b
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-06-02 16:41:06 +0200

    Max life density is done

--- src/life.cpp
+++ src/life.cpp
@@ -11,7 +11,7 @@ public:
 	Life(const SizeOptions& opt) :
 			Script(opt), n(opt.size()), gridVector(*this, (n + 4) * (n + 4), 0, 1) {
 
-		Matrix<BoolVarArray> grid(gridVector, n + 4, n + 4);
+		Matrix<BoolVarArgs> grid(gridVector, n + 4, n + 4);
 
 		// Borders should be zero
 		rel(*this, grid.row(0), IRT_EQ, 0);
@@ -24,36 +24,48 @@ public:
 		rel(*this, grid.col(n + 2), IRT_EQ, 0);
 		rel(*this, grid.col(n + 3), IRT_EQ, 0);
 
-		for (int i = 2; i < n; i++) {
-			for (int j = 2; j < n; j++) {
-				BoolVar current = grid(i, j);
-				BoolVar up = grid(i, j - 1);
-				BoolVar down = grid(i, j + 1);
-				BoolVar left = grid(i - 1, j);
-				BoolVar right = grid(i + 1, j);
+		for (int i = 1; i < n + 3; i++) {
+			for (int j = 1; j < n + 3; j++) {
+				BoolVarArray neighbors(*this, 8, 0, 1);
 
-				std::cout << "UP: " << up << "DOWN: " << down << "LEFT: " << left << "RIGHT: " << right << std::endl;
+				// Up
+				neighbors[0] = grid(i, j - 1);
+				// Down
+				neighbors[1] = grid(i, j + 1);
+				// Left
+				neighbors[2] = grid(i - 1, j);
+				// Right
+				neighbors[3] = grid(i + 1, j);
 
-				// TODO None is alive. It is NOT working
-				if (current.one()) {
-					// Keep the cell alive
-					std::cout << "ALIVE" << std::endl;
-					rel(*this, up + down + left + right <= 3);
-					rel(*this, up + down + left + right >= 2);
-				} else {
-					std::cout << "DEAD" << std::endl;
-					rel(*this, up + down + left + right < 2);
-				}
+				// Up right
+				neighbors[4] = grid(i + 1, j - 1);
+				// Up left
+				neighbors[5] = grid(i - 1, j - 1);
+				// Down right
+				neighbors[6] = grid(i + 1, j + 1);
+				// Down left
+				neighbors[7] = grid(i - 1, j + 1);
+
+				IntVar alive(*this, 0, 8);
+
+				rel(*this, sum(neighbors) == alive);
+
+				// Alive cells should remain alive
+				rel(*this, grid(i, j) >> (alive >= 2));
+				rel(*this, grid(i, j) >> (alive <= 3));
+
+				// Dead cells should remain dead
+				rel(*this, !grid(i, j) >> (alive != 3));
 			}
 		}
 
-		branch(*this, gridVector, INT_VAR_MIN_MAX(), INT_VAL_MIN());
+		branch(*this, gridVector, INT_VAR_SIZE_MAX(), INT_VAL_MAX());
+
 	}
 
 	// Constructor for cloning
 	Life(bool share, Life& life, int size) :
 			Script(share, life), n(size) {
-		//x.update(*this, share, square.x);
 		gridVector.update(*this, share, life.gridVector);
 	}
 
@@ -64,18 +76,32 @@ public:
 	}
 
 	virtual void print(std::ostream& os) const {
+		int counter = 0;
+
 		for (int i = 1; i <= gridVector.size(); i++) {
 			std::cout << gridVector[i - 1] << " ";
+			if (gridVector[i -1].val() == 1)
+				counter++;
 			if ((i % (n + 4)) == 0) {
 				std::cout << std::endl;
 			}
 		}
+
+		std::cout << "Sum: " << counter << std::endl;
+
+		std::cout << std::endl;
+	}
+
+	virtual void constrain(const Space& _home) {
+		const Life& home = static_cast<const Life&>(_home);
+		rel(*this, sum(gridVector) > sum(home.gridVector));
 	}
 };
 
 int main(int argc, char* argv[]) {
 	SizeOptions opt("Game of Life");
-	opt.size(7);
+	opt.size(8);
+	opt.solutions(0);
 	opt.parse(argc, argv);
 	Script::run<Life, BAB, SizeOptions>(opt);
 	return 0;
